35a549ebeab1e69260422708fe4e4d2afeb91f49
saad <saadfathallah420@gmail.com>
2023-04-08 15:41:13 +0000

    validation email password

---
+++ test/tp_validation_email_password_test.dart
@@ -0,0 +1,8 @@
+import 'package:tp_validation_email_password/tp_validation_email_password.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
